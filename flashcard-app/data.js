d86c920207315bb1e8be5d632dd7d4eaf92bd12d
haijin2 <94418542+haijin2@users.noreply.github.com>
2023-11-13 19:25:58 +0800

    commit

---
+++ flashcard-app/data.js
@@ -0,0 +1,20 @@
+const details =[
+    {
+        title:"Preview one",
+        author:"bile",
+    },
+    {
+        title:"Preview two",
+        author:"bile",
+    },
+    {
+        title:"Preview three",
+        author:"bile",
+    },
+    {
+        title:"Preview four",
+        author:"bile",
+    },
+];
+
+export default details;
